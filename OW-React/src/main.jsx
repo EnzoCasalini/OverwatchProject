ae55dc3ba77a64283374c852e2ad1a82bd26ab15
Andréas CASTELLO <andreas.castello06@gmail.com>
2023-04-05 03:00:34 +0200

    Profile part

--- OW-React/src/main.jsx
+++ OW-React/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import PlayerPage from "./PlayersPage/PlayersPage";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+      <PlayerPage />
   </React.StrictMode>,
 )
